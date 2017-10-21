fcef42bb8676713455fd4777d713b071580a79d4
Ildyakov Alexander <a.ildjakov@yandex.ru>
2017-10-22 00:09:25 +0700

    Homework 03

--- scripts/build-images.sh
+++ scripts/build-images.sh
@@ -4,6 +4,6 @@ docker pull mongo:latest
 
 docker build -t drsklif/post:1.0 ./post-py
 
-docker build -t drsklif/comment:2.0 ./comment
+docker build -t drsklif/comment:1.0 ./comment
 
-docker build -t drsklif/ui:2.0 ./ui
+docker build -t drsklif/ui:1.0 ./ui
